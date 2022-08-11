c99cfb0d90d707a93d5491036ad331f670c9ed14
ShaunB21 <99273244+ShaunB21@users.noreply.github.com>
2022-08-11 15:04:49 +0100

    Added a help command

---
+++ commands/help.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'help',
+    description: "Connect four game",
+    async execute(interaction, args) {
+        interaction.channel.send('Commands:' +
+            '\n' + 'n.start to start a game' +
+            '\n' + 'n.join to join a game' +
+            '\n' + 'n.begin to begin a game' +
+            '\n' + 'n.move to make your move')
+    }
+
+}
